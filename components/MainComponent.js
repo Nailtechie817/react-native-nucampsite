18193bd9d27d0ae441b55d347cc21c14b7e12a77
Nailtechie817 <britmt817@gmail.com>
2022-05-20 15:06:56 -0400

    Modal

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -251,4 +251,21 @@ class Main extends Component {
     }
 }
 
+const styles = StyleSheet.create({
+    formRow: {
+        alignItems: 'center',
+        justifyContent: 'center',
+        flex: 1,
+        flexDirection: 'row',
+        margin: 20
+    },
+    formLabel: {
+        fontSize: 18,
+        flex: 2
+    },
+    formItem: {
+        flex: 1
+    }
+});
+
     export default connect(mapStateToProps)(Home);
